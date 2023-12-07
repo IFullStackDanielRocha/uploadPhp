a357b95b5a33258cf57ae382eb730c667d623851
Daniel Rocha <ideveloper.fstack@gmail.com>
2023-12-07 16:59:06 -0300

    Multiplos arquivos e delete

--- index.php
+++ index.php
@@ -1,15 +1,89 @@
 <?php
 include("connection.php");
 // Seguraça verificar se usuário está logado para envio de arquivos
-//DEBUG
-/*
-echo"<pre>";
-var_dump($_FILES);
-echo "</pre>";*/
 
-if(count($_FILES) > 0){
+if(isset($_GET['delete'])){
+
+    $id = intval($_GET['delete']);
+    $sql = $mysqli->query("SELECT * FROM upload WHERE id= '$id'limit 1") or die(mysqli_error($mysqli));
+    $row = $sql->fetch_assoc();
     
+    if(unlink($row['path'])){
+        $success= $mysqli->query("DELETE FROM upload WHERE id= '$id'") or die(mysqli_error($mysqli));
+        if($success){
+            echo "Arquivo excluido com sucesso!";
+        }
+    };
+
+
 }
+function sendFile($error,$size,$name,$tmp_Name){
+
+    include("connection.php");
+
+
+    if($error){
+        die('Falha ao enviar arquivos!');
+    };
+
+
+
+    if($size > 2097152){
+        die('Arquivo gigante! Máximo: 2MB');
+    }
+
+    $localsave = "arquivos/";
+    $fileName = $name;
+    $newName = uniqid();
+
+    //$extension = pathinfo($fileName, PATHINFO_EXTENSION);
+    $extension = strtolower(pathinfo($fileName, PATHINFO_EXTENSION));
+
+    if($extension !="jpg" && $extension != "png"){
+        die ("Tipo de arquivo não permitido!");
+   
+    }
+    $path = $localsave. $newName .".".$extension;
+    $saved = move_uploaded_file($tmp_Name, $path);
+
+
+
+    if($saved){
+        
+        $mysqli->query("INSERT INTO upload(path,data,name) VALUES('$path',Now(),'$fileName')")
+        or die($mysqli->error);
+        return true;
+    }else{
+       return false;
+    }
+
+
+}
+
+//var_dump($_FILES);
+if(isset($_FILES['upload'])){
+    $myFile = $_FILES['upload'];
+    $working=true;
+    foreach ($myFile['name'] as $index =>$arqs) {
+        $itsWorking = sendFile($myFile['error'][$index], $myFile['size'][$index], $myFile['name'][$index], $myFile['tmp_name'][$index]);
+        if(!$working){   
+            $working=false;
+        };
+    }
+    if($itsWorking){
+        echo "Arquivos salvos com sucesso";
+    }else{
+        echo "Arquivos não salvos error";
+    }
+
+    
+
+    
+
+}
+
+
+/*
 
 if(isset($_FILES['upload'])){
     $myFile = $_FILES['upload'];
@@ -53,7 +127,7 @@ if(isset($_FILES['upload'])){
     }
         
     
-}
+}*/
 
 
 $sql_query = $mysqli->query("SELECT * FROM upload") or die($mysqli->error);
@@ -82,6 +156,7 @@ $sql_query = $mysqli->query("SELECT * FROM upload") or die($mysqli->error);
                 <th>Previews</th>
                 <th>Arquivo</th>
                 <th>Data de envio</th>
+                <th>Apagar arquivo</th>
             </tr>
             <tbody>
             <?php
@@ -92,6 +167,7 @@ $sql_query = $mysqli->query("SELECT * FROM upload") or die($mysqli->error);
                     <td><img src="<?php echo $row['path']; ?>" alt="" width="36px"></td>
                     <td><?php echo $row['name']; ?></td>
                     <td><?php echo date("d/m/Y H:i", strtotime($row['data'])); ?></td>
+                    <td><a href="index.php?delete=<?php echo $row['id'];?>"><button style="color: yellow; background-color:red; width:60px; height:30px"><strong>Deletar</strong></button></a> </td>
                 </tr>
                 <?php } ?>
 
